0cadfcb98d0d4ad660e3b9c7f37be680e19260e4
pavibyte <learnpavi123@gmail.com>
2025-12-11 19:58:56 +0530

    coded preprocessing

---
+++ src/preprocessing/preprocess.py
@@ -0,0 +1,41 @@
+# src/preprocessing/preprocess.py
+import pandas as pd
+import joblib
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.decomposition import PCA
+import os
+
+def load_data(path=r"C:\Users\VICTUS\OneDrive\Desktop\FINAL YEAR PROJECT\EEG Deep Learning project\data\mental-state.csv"):
+    df = pd.read_csv(path)
+    X = df.drop("Label", axis=1)
+    y = df["Label"].astype(int)
+    return X, y
+
+def preprocess_split(path=r"C:\Users\VICTUS\OneDrive\Desktop\FINAL YEAR PROJECT\EEG Deep Learning project\data\mental-state.csv", test_size=0.2, random_state=42,
+                     do_pca=False, pca_components=200, save_dir="saved_models"):
+    os.makedirs(save_dir, exist_ok=True)
+    X, y = load_data(path)
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=test_size, stratify=y, random_state=random_state
+    )
+
+    scaler = StandardScaler()
+    X_train_scaled = scaler.fit_transform(X_train)
+    X_test_scaled = scaler.transform(X_test)
+
+    joblib.dump(scaler, f"{save_dir}/scaler.joblib")
+
+    pca = None
+    if do_pca:
+        pca = PCA(n_components=pca_components, random_state=random_state)
+        X_train_scaled = pca.fit_transform(X_train_scaled)
+        X_test_scaled = pca.transform(X_test_scaled)
+        joblib.dump(pca, f"{save_dir}/pca.joblib")
+
+    return X_train_scaled, X_test_scaled, y_train.values, y_test.values
+
+if __name__ == "__main__":
+    X_tr, X_te, y_tr, y_te = preprocess_split()
+    print("X_train shape:", X_tr.shape)
+    print("X_test shape:", X_te.shape)
